9bac9996b1d4d2075da288d57d7a67a9d9b3340b
Muhammad Hamdan <muhammad.hamdan@intel.com>
2024-01-24 15:10:39 -0800

    Clean 2

--- unittest/unittest.py
+++ unittest/unittest.py
@@ -1,8 +1,5 @@
 import os
 from CustomXMLParser import XmlParser
-from dicttoxml import dicttoxml as dtx
-import xml.dom.minidom
-from typing import Dict, List
 
 tdx_file = os.path.join('unittest', 'test_data', 'test_dx.XML')
 config_file = os.path.join('unittest', 'test_data', 'CONFIG_EXAMPLE.json')
@@ -11,10 +8,6 @@ parser = XmlParser(config_file=config_file, parser_type='custom', verbose=False)
 data = parser.parse(tdx_file)
 
 out_tdx = tdx_file.replace('.XML', '_mod.XML')
-# xmlout = parser.dumps(data, input_format='custom', root='root')
 parser.dump(data, out_tdx, input_format='custom', root='root')
 
-# with open(out_tdx, 'w') as of:
-#     of.write(xmlout)
-
-print()
+print("Done!!")
